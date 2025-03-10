b1ae5abbcb55b945f5dab4a0f5bc15a8e2aa64c9
leonardofidelis@youbrtech.com.br <leofipp@hotmail.com>
2025-03-10 17:49:17 -0300

    reprocessando pedidos

--- Design_Patterns/Goroutines_Channels/Example_1/main.go
+++ Design_Patterns/Goroutines_Channels/Example_1/main.go
@@ -13,7 +13,7 @@ type Order struct{
     Description string
 }
 
-func processOrder(ctx context.Context ,workerId int ,orderChannel <-chan Order , wg *sync.WaitGroup , workerPool chan struct{}){
+func processOrder(ctx context.Context ,workerId int ,orderChannel <-chan Order , unprocessedChannel chan <- Order , wg *sync.WaitGroup , workerPool chan struct{}){
     defer wg.Done()
 
     for {
@@ -22,21 +22,30 @@ func processOrder(ctx context.Context ,workerId int ,orderChannel <-chan Order ,
             if !ok {
                 return
             }
+            
+            fmt.Printf("Pedido %d recebido\n" , order.Id)
+            
             select {
             case workerPool <- struct{}{}:
             case <-ctx.Done():
                 fmt.Printf("Worker %d cancelado antes de processar o pedido %d\n", workerId, order.Id)
+                unprocessedChannel<-order
                 return
             }
+            
             fmt.Printf("Worker: %d\t Procesando o pedido ID:%d\n" ,workerId, order.Id)
+
             select {
             case <-time.After(2 * time.Second):
                 fmt.Println("Pedido processado")
             case <- ctx.Done():
                 fmt.Printf("worker cancelado enquanto processava %d\n",order.Id)
+                unprocessedChannel<-order
                 return
             }
+            
             <-workerPool
+            
         case <-ctx.Done():
             fmt.Println("Worker cancelado")
             return
@@ -44,26 +53,43 @@ func processOrder(ctx context.Context ,workerId int ,orderChannel <-chan Order ,
     }
 }
 
-func main(){
-    workers := 3
-    orderChannel := make(chan Order,10)
-    workerPool := make(chan struct{},workers)
+func handleProcessOrders(orderChannel , unprocessedChannel chan Order,workerPool chan struct{}){
     var wg sync.WaitGroup
 
-    ctx , cancel := context.WithTimeout(context.Background(),2*time.Second)
+    ctx , cancel := context.WithTimeout(context.Background(),10*time.Second)
     defer cancel()
-
+    
     for i := 0; i <= 5 ; i++{
         wg.Add(1)
-        go processOrder(ctx ,i ,orderChannel,&wg ,workerPool)
+        go processOrder(ctx ,i ,orderChannel,unprocessedChannel,&wg ,workerPool)
     }
 
     for i:= 0 ; i < 15 ; i++{
-        fmt.Printf("Pedido %d recebido\n" , i)
         orderChannel<-Order{Id:i,Amount: float64(i) * 10.0,Description:fmt.Sprintf("Pedido %d" , i)}
     }
 
     close(orderChannel)
     wg.Wait()
+    close(unprocessedChannel)
+
+}
+
+
+
+func main(){
+    workers := 3
+    orderChannel := make(chan Order,10)
+    unprocessedChannel := make(chan Order,10)
+    workerPool := make(chan struct{},workers)
+
+    handleProcessOrders(orderChannel,unprocessedChannel,workerPool);
+    
+    fmt.Println("Pedidos não processados:")
+    for order := range unprocessedChannel {
+        fmt.Printf("Id do pedido: %d\n" ,order.Id)
+    }
+
+        
+  
     fmt.Println("Pedidos finalizados")
 }
